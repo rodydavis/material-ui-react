0ad6aa5419671b2c12fedcf552b1ca4003c09823
Rody Davis <31253215+rodydavis@users.noreply.github.com>
2020-08-18 17:03:30 -0700

    adding base project

---
+++ docs/precache-manifest.6e377f392e3b8e94e1693b15032b606e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86a83760cf00130b763597189d731d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5073cec29f9db6e9118c",
+    "url": "/static/js/2.d1ba0a11.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d1ba0a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7334873c6b15e5787b6e",
+    "url": "/static/js/main.112b3cd8.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  }
+]);
